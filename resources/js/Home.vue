819ef5a0329cc0a151d2e96d2fded0963f03167f
TheoMuller510 <t.muller.67400@gmail.com>
2024-04-11 18:52:47 +0200

    couche de style

--- resources/js/Home.vue
+++ resources/js/Home.vue
@@ -68,11 +68,12 @@ const resetProduct = () => {
         image: ''
     };
 };
+
 </script>
 
 <template>
     <div class="container">
-        <div class="row flex justify-center space-x-20">
+        <div class="row flex justify-center space-x-20 mx-8">
             <div class="col-md-4">
                <div class="flex">
 				<h2 class="font-bold uppercase text-4xl">Ajoutez un <br/> article</h2>
@@ -88,10 +89,10 @@ const resetProduct = () => {
                         <div class="form-group">
                             <input type="text" v-model="product.category" class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" placeholder="Catégorie de l'article">
                         </div>
-                        <!-- <div class="form-group">
+                        <div class="form-group">
                             <label>Image</label>
                             <input type="file" v-on:change="product.image" class="form-control-file">
-                        </div> -->
+                        </div>
                         <div class="flex space-x-2 mt-2">
                         <button type="submit" class="uppercase text-sm font-bold tracking-wide bg-blue-900 text-gray-100 p-3 rounded-lg w-full 
                       focus:outline-none focus:shadow-outline">Enregistrer</button>  
@@ -112,7 +113,7 @@ const resetProduct = () => {
                             <th scope="col" class="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">Description</th>
                             <th scope="col" class="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">Catégorie</th>
                             <th scope="col" class="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">Image</th>
-                            <th scope="col" class="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">Options</th>
+                            <th scope="col" class="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50 flex justify-center">Options</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -121,9 +122,11 @@ const resetProduct = () => {
                             <td class="p-4 border-b border-blue-gray-50">{{ product.description }}</td>
                             <td class="p-4 border-b border-blue-gray-50">{{ product.category }}</td>
                             <td class="p-1 border-b border-blue-gray-50"><img src="https://source.unsplash.com/random/100x100?appartment" :alt="product.name"></td>
-                            <td class="p-4 mt-7 flex space-x-4">
+                            <td class="p-1 border-b border-blue-gray-50">
+                                <div class="space-x-1 text-center">
                                 <button type="button"  @click="editProduct(product)"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><polygon points="16 3 21 8 8 21 3 21 3 16 16 3"></polygon></svg></button>
                                 <button type="button"  @click="deleteProduct(product)"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="#ff0000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></button>
+                            </div>
                             </td>
                         </tr>
                     </tbody>
